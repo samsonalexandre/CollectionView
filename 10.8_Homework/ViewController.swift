8ad364fd48a568da8b29fb4a1310a4f34f26d9be
Alexandre Samson <alekssamson@gmail.com>
2021-06-11 20:25:57 +0200

    Initial Commit

---
+++ 10.8_Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10.8_Homework
+//
+//  Created by Alexandre Samson on 11.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
